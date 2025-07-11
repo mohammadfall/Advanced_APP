f4f067a8c6fc14dcae596a13fbb78317cb4da3b8
mohammadfall <alrothematics.mohammad0@gmail.com>
2025-07-11 11:19:41 +0300

    Update streamlit_advanced_pdf_app.py

--- streamlit_advanced_pdf_app.py
+++ streamlit_advanced_pdf_app.py
@@ -68,7 +68,7 @@ if not creds or not creds.valid:
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
             "client_secret.json", SCOPES)
-        creds = flow.run_local_server(port=0)
+        creds = flow.run_console()
     with open("token.pickle", "wb") as token:
         pickle.dump(creds, token)
 
